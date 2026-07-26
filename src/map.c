8abb9d84eb96d52416a42346ecb9655fd263528b
Simon Brakhane <simon@brakhane.net>
2026-07-26 20:04:46 +0200

    decomp(map): recover clear-area continue scope
    Forward clear_an_area 1:1 from caesar2 43bcdf4c. The complete reconstructed PS.EXE remains byte-identical.

--- src/map.c
+++ src/map.c
@@ -2189,8 +2189,8 @@ void clear_an_area(int x1, int y1, int x2, int y2)
                     (*(struct city_cell *)((unsigned char *)city_map + (cm_sptr))).base_kind = (*(struct city_cell *)((unsigned char *)city_map + (cm_sptr))).building;
                     (*(struct city_cell *)((unsigned char *)city_map + (cm_sptr))).terrain &= 0xdf;
                     (*(struct city_cell *)((unsigned char *)city_map + (cm_sptr))).building = 0;
-                    continue;
                 }
+                continue;
             } else {
 
                 if ((*(struct city_cell *)((unsigned char *)city_map + (cm_sptr))).base_kind >= 0x82) {
