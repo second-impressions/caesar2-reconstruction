e6af110f16204fe75bfa005bf16af4c7ffdd53bd
Simon Brakhane <simon@brakhane.net>
2026-07-29 21:21:35 +0200

    fight_barbarian: recover named combat strength
    Forward-port the dual-target exact source form from the decompilation repository.

--- src/int_c2.c
+++ src/int_c2.c
@@ -1820,9 +1820,12 @@ void fight_centurian(int other_idx)
 // FUNCTION: C2WIN 0x0040aac6
 void fight_barbarian(int other_idx)
 {
-    if (citizen_list[other_idx].xp + 2 < rand8) citizen_list[citizen_no].state_idx = 2;
+    int strength;
+
+    strength = citizen_list[other_idx].xp + 2;
+    if (strength < rand8) citizen_list[citizen_no].state_idx = 2;
     else                                  citizen_list[other_idx].state_idx        = 2;
-    citizen_list[other_idx].xp += 1;
+    citizen_list[other_idx].xp++;
 }
 
 // Move citizen one cell in citizen.world_dir (0..7).
