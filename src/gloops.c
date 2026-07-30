af2559b077018e1e8f861ca25436294664419708
Simon Brakhane <simon@brakhane.net>
2026-07-30 07:59:10 +0200

    gloops(mloop_end): match the Windows refresh path
    Retain set_mouse_refresh on non-Windows targets while matching the shipped Windows modal helper.

--- src/gloops.c
+++ src/gloops.c
@@ -55,7 +55,9 @@ void mloop_end(void)
 {
     get_mouse_droppings();
     show_mouse(pointer_mode);
+#if !PLATFORM_WINDOWS
     set_mouse_refresh();
+#endif
     refresh_svga_screen();
     continue_db();
     button_time_flag = running_delay1();
