ab4989df786a8f225be5b00df46e14aaf2065320
Simon Brakhane <simon@brakhane.net>
2026-08-02 19:52:35 +0200

    lib32(get_filename_extension): recover shared byte walk
    Recover the unsigned delimiter byte, positive dot-match block, and post-increment on the third extension read shared by the DOS and Windows builds.

--- src/lib32.c
+++ src/lib32.c
@@ -445,19 +445,20 @@ void main_path(void)
 // Walk past the filename to the '.' separator and copy the 3-char extension into the global
 // `extension[]` buffer (NUL-terminated).
 // FUNCTION: C2 0x243b0
+// FUNCTION: C2WIN 0x0044aab8
 void get_filename_extension(const char *filename)
 {
-    char character;
+    unsigned char character;
     do {
         character = *filename;
         filename++;
     } while (character != '.' && character != 0);
-    if (character != '.')
-        return;
-    extension[0] = *filename++;
-    extension[1] = *filename++;
-    extension[2] = *filename;
-    extension[3] = 0;
+    if (character == '.') {
+        extension[0] = *filename++;
+        extension[1] = *filename++;
+        extension[2] = *filename++;
+        extension[3] = 0;
+    }
 }
 
 // Walk past the filename to the '.' separator and overwrite the 3-char extension with the global
