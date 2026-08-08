ee23baa5e412bef709007b53eac4dfa77d8c7c0a
Simon Brakhane <simon@brakhane.net>
2026-08-08 16:15:01 +0200

    match(lib32): recover format whitespace declaration
    Forward the recovered strip_fb_spaces declaration in the string-helper prototype block.

--- src/lib32.c
+++ src/lib32.c
@@ -2192,6 +2192,7 @@ void clear_keys(void)
 
 void pull_string_left(char *p, char *end);
 void push_string_right(char *start, char *end);
+void strip_fb_spaces(void);
 unsigned char to_upper(unsigned char c);
 
 // Bounded byte-compare of two strings: returns 1-based index of first mismatch, or 0 if all `n`
