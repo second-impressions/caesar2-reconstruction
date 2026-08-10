a17876eaeb282e09ae05510742d7e7718d1256bf
Simon Brakhane <simon@brakhane.net>
2026-08-10 14:14:23 +0200

    fix(c2): forward Windows battle buffer initialization
    Forward the dual-target exact init_battle_gfx_buffers body. The strict DOS rebuild remains 1435/1435 game functions exact with zero differing code bytes.

--- src/c2.c
+++ src/c2.c
@@ -921,6 +921,35 @@ void clear_map_gfx_buffers(void)
 // Marks all battle graphics buffers as empty.
 // FUNCTION: C2 0x10d80
 // FUNCTION: C2WIN 0x004451bf
+#if PLATFORM_WINDOWS
+void init_battle_gfx_buffers(void)
+{
+    if (fixt_data) free(fixt_data);
+    fixt_data = 0;
+    if (figure1_data) free(figure1_data);
+    figure1_data = 0;
+    if (figure2_data) free(figure2_data);
+    figure2_data = 0;
+    if (figure3_data) free(figure3_data);
+    figure3_data = 0;
+    if (figure4_data) free(figure4_data);
+    figure4_data = 0;
+    if (figure5_data) free(figure5_data);
+    figure5_data = 0;
+    if (figure6_data) free(figure6_data);
+    figure6_data = 0;
+    if (figure7_data) free(figure7_data);
+    figure7_data = 0;
+    if (figure8_data) free(figure8_data);
+    figure8_data = 0;
+    if (figure9_data) free(figure9_data);
+    figure9_data = 0;
+    if (figure10_data) free(figure10_data);
+    figure10_data = 0;
+    city_gfx_loaded = 0;
+    city_gfx_zoom = 0xff;
+}
+#else
 void init_battle_gfx_buffers(void)
 {
     fixt_data     = 0;
@@ -935,6 +964,7 @@ void init_battle_gfx_buffers(void)
     figure9_data  = 0;
     figure10_data = 0;
 }
+#endif
 
 // Releases all allocated battle graphics buffers.
 // FUNCTION: C2 0x10dc7
