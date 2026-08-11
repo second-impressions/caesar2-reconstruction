311045919ac0e8c45a49df368db376bdd74b5209
Simon Brakhane <simon@brakhane.net>
2026-08-11 15:42:01 +0200

    mmedia: restore media flag source grouping
    Forward-port the dual-target byte-exact load_media_entry source from the decompilation corpus.

--- src/mmedia.c
+++ src/mmedia.c
@@ -196,24 +196,16 @@ void launch_help(int page)
 // FUNCTION: C2WIN 0x00452332
 void load_media_entry(void)
 {
-    media_voc = 0;
-    media_right_image = 0;
-    media_left_image = 0;
+    media_left_image = media_right_image = media_voc = 0;
 
     readfile(media_file, &this_media_entry, 0x3a,
              this_help_page * 0x3a + 8);
     readfile(media_file, format_buffer, 0x7d0,
              this_media_entry.text_offset);
 
-    if (my_strcmp(this_media_entry.left_file, "null.pl8", 8) != 0) {
-        media_left_image = 1;
-    }
-    if (my_strcmp(this_media_entry.right_file, "null.pl8", 8) != 0) {
-        media_right_image = 1;
-    }
-    if (my_strcmp(this_media_entry.voc_file, "null.voc", 8) != 0) {
-        media_voc = 1;
-    }
+    if (my_strcmp(this_media_entry.left_file, "null.pl8", 8) != 0) media_left_image = 1;
+    if (my_strcmp(this_media_entry.right_file, "null.pl8", 8) != 0) media_right_image = 1;
+    if (my_strcmp(this_media_entry.voc_file, "null.voc", 8) != 0) media_voc = 1;
 }
 
 // Render the in-game F1 help dialog.
